87510dc7e57f09b003d7d2439ab508c79d99bf6c
Pat Allan <pat@freelancing-gods.com>
2013-12-17 17:21:29 +1100

    Helpful helpers for crafting Like links.

---
+++ app/helpers/like/likes_helper.rb
@@ -0,0 +1,16 @@
+module Like::LikesHelper
+  def like_path_for(likeable)
+    like_likes_path likeable_to_params(likeable)
+  end
+
+  def like_url_for(likeable)
+    like_likes_url likeable_to_params(likeable)
+  end
+
+  def likeable_to_params(likeable)
+    {
+      likeable_type: likeable.class.name,
+      likeable_id:   likeable.id
+    }
+  end
+end
